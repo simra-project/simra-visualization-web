046a4a0b98dfe5dda482967cfc9fc189f1e6ef3b
Sanjeet Raj Pandey <sanjeet.r.pandey@campus.tu-berlin.de>
2019-10-28 21:40:49 +0100

    first commit, maven space created. .gitignore added Log4j added.

---
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/CSVImporter.java
@@ -0,0 +1,15 @@
+package main.java.com.simra.app.csvimporter;
+
+
+import org.apache.log4j.Logger;
+
+public class CSVImporter {
+
+    private static final Logger logger = Logger.getLogger(CSVImporter.class);
+
+
+    public static void main(String[] args) {
+        logger.info("Hello, World !!!");
+    }
+
+}
