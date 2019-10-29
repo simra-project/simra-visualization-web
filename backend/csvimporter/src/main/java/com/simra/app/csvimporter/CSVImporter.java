80f9849a5fa6373465e170d970f04c89ce3986f0
spandey <SanjeetRaj.Pandey@init.de>
2019-10-29 13:11:00 +0100

    * argument parser updated.

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/CSVImporter.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/CSVImporter.java
@@ -6,23 +6,15 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ByteChannel;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
-import java.nio.file.StandardOpenOption;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class CSVImporter {
 
     private static final Logger logger = Logger.getLogger(CSVImporter.class);
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArgumentParserException {
         ArgumentParser parser = ArgumentParsers.newFor("CSVImporter").build()
                 .defaultHelp(true)
                 .description("Import CSV file.");
@@ -30,7 +22,7 @@ public class CSVImporter {
                 .choices("r", "p").setDefault("r")
                 .help("Specify file type r <- ride, p <- profile");
         parser.addArgument("file").nargs("*")
-                .help("File to calculate checksum");
+                .help("File to read");
         Namespace ns = null;
         try {
             ns = parser.parseArgs(args);
@@ -38,28 +30,11 @@ public class CSVImporter {
             parser.handleError(e);
             System.exit(1);
         }
-        MessageDigest digest = null;
-        try {
-            digest = MessageDigest.getInstance(ns.getString("type"));
-        } catch (NoSuchAlgorithmException e) {
-            logger.error(String.format("Could not get file type %s: %s",
-                    ns.getString("type"), e.getMessage()));
-            System.exit(1);
-        }
+        String type= ns.getString("type");
+        if(type.isEmpty()) throw new ArgumentParserException(parser);
         for (String name : ns.<String> getList("file")) {
             Path path = Paths.get(name);
-            try (ByteChannel channel = Files.newByteChannel(path,
-                    StandardOpenOption.READ)) {
-                ByteBuffer buffer = ByteBuffer.allocate(4096);
-                while (channel.read(buffer) > 0) {
-                    buffer.flip();
-                    digest.update(buffer);
-                    buffer.clear();
-                }
-            } catch (IOException e) {
-                logger.error("failed to read data: ", e);
-            }
-
+            logger.info(path.getFileName());
         }
 
     }
