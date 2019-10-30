f42daa636252430ffc0d5aba3d2862e07901cc6d
Sanjeet Raj Pandey <sanjeet.r.pandey@campus.tu-berlin.de>
2019-10-30 08:42:50 +0100

    Profile model added.

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/handler/ProfileFileIOHandler.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/handler/ProfileFileIOHandler.java
@@ -1,6 +1,7 @@
 package main.java.com.simra.app.csvimporter.handler;
 
 
+import main.java.com.simra.app.csvimporter.model.Profile;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
@@ -11,8 +12,11 @@ import java.nio.file.Path;
 public class ProfileFileIOHandler extends FileIOHandler {
     private static final Logger logger = Logger.getLogger(ProfileFileIOHandler.class);
 
+    private static final String FILEVERSIONSPLITTER= "#";
+
     public ProfileFileIOHandler(Path path) {
         super(path);
+        this.fileParse();
     }
 
     @Override
@@ -20,10 +24,24 @@ public class ProfileFileIOHandler extends FileIOHandler {
         try (BufferedReader reader =
                      new BufferedReader(new FileReader(String.valueOf(this.getPath())))) {
             String line= reader.readLine();
+            String[] arrOfStr = line.split(FILEVERSIONSPLITTER);
+
+            Profile profile= new Profile();
+            profile.setAppVersion(arrOfStr[0]);
+            profile.setFileVersion(Integer.parseInt(arrOfStr[1]));
+
+
+            StringBuilder profileCSVwithHeader = new StringBuilder();
             while (line != null) {
 
                 line = reader.readLine();
+                if(line != null){
+                    profileCSVwithHeader.append("\n");
+                    profileCSVwithHeader.append(line);
+                }
+
             }
+            logger.info(profileCSVwithHeader.toString());
 
         }catch (IOException e){
             logger.error(e);
