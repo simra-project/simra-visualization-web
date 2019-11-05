dc1f768314d0c0e20793759f05a0d5e77bcba955
Adrian Gronau <adrian.gronau@gmx.de>
2019-11-05 21:54:37 +0100

    Fixed gitignore and re-added some .idea files

--- backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/IncidentRepositoryImpl.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/IncidentRepositoryImpl.java
@@ -22,7 +22,7 @@ public class IncidentRepositoryImpl implements IncidentRepository {
     @Override
     public RideResource getAllRideIncidents() {
 
-        HashMap<String, Double> map = new HashMap();
+        HashMap<String, Double> map = new HashMap<>();
         map.put("lat", 52.512830);
         map.put("long", 13.322887);
 
@@ -34,10 +34,9 @@ public class IncidentRepositoryImpl implements IncidentRepository {
         incidents.add(incidentResource2);
         incidents.add(incidentResource3);
 
-        RideResource ride = new RideResource(1, incidents);
         //String incident = "{\n            id: \"Incident 1\",\n            latlng: {\n                lat: 52.512830,\n                lng: 13.322887\n            },\n            description: \"Auto hat mich beim Einfaedeln fast mitgenommen!\"\n        },\n        {\n            id: \"Incident 2\",\n            latlng: {\n                lat: 52.512719,\n                lng: 13.324711\n            },\n            description: \"Wurde von ein paar Vertretern auf ein Jobangebot angesprochen...\"\n        },\n        {\n            id: \"Incident 3\",\n            latlng: {\n                lat: 52.509777,\n                lng: 13.325281\n            },\n            description: \"Viel zu lange Schlangen in der Mensa.\"\n        }";
 
-        return ride;
+        return new RideResource(1, incidents);
     }
 
 
