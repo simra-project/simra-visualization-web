3e5227db07030d91d0a6da7c4fc501de23685a8f
Richard Viehoever <richard.viehoever@gmail.com>
2019-11-20 15:56:06 +0100

    Fix CORS errors

--- backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/RideController.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/RideController.java
@@ -3,10 +3,7 @@ package visualization.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import visualization.service.RideService;
 import visualization.web.resources.RideResource;
 
@@ -18,6 +15,8 @@ import java.util.List;
 This is the place where we communicate with the frontend
 
  */
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class RideController {
 
