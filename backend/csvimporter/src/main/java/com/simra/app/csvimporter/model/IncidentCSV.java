11f1428365cace594fc5c5a154a8d316e629299a
konskoehler <kons.koehler@gmail.com>
2019-11-21 16:08:59 +0100

    Update IncidentCSV.java
    fix bug

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/model/IncidentCSV.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/model/IncidentCSV.java
@@ -463,7 +463,7 @@ public class IncidentCSV extends ApplicationFileVersion implements MongoDocument
         Document document = new Document();
 
         Document contentCompositeId = new Document();
-        contentCompositeId.put("rideId", this.getFileId());
+        document.put("rideId", this.getFileId());
         contentCompositeId.put("rideId", this.getFileId());
         contentCompositeId.put("key", this.key);
         document.put("_id", contentCompositeId);
