b62812df9812ac05b009586b820772b22eade879
7imo <timokraus92@gmail.com>
2019-11-25 17:43:31 +0100

    cors for statistics endpoint

--- backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/StatisticsController.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/StatisticsController.java
@@ -3,6 +3,7 @@ package visualization.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import java.util.List;
 This is the place where we communicate with the frontend
 
  */
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class StatisticsController {
 
