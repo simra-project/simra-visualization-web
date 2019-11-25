0fdf443b91b9a98b763cbd92e27c37ace0eadcd6
7imo <timokraus92@gmail.com>
2019-11-25 16:42:50 +0100

    polygon geo query

--- backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/IncidentRepository.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/IncidentRepository.java
@@ -1,6 +1,5 @@
 package visualization.data.mongodb;
 
-import org.springframework.data.geo.Box;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.geo.GeoJsonPolygon;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -16,7 +15,6 @@ public interface IncidentRepository extends MongoRepository<IncidentEntity, Inci
     Optional<IncidentEntity> findById(IncidentEntity.CompositeKey id);
 
     List<IncidentEntity> findByLocationNear(GeoJsonPoint coordinates, int maxDistance);
-    //https://docs.mongodb.com/manual/reference/operator/query/box/
 
     List<IncidentEntity> findByLocationWithin(GeoJsonPolygon polygon);
 
