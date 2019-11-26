646702d46be2832ea40658b6d05526556b4a02df
spandey <SanjeetRaj.Pandey@init.de>
2019-11-26 13:25:51 +0100

    updating Log in CSVImporter.

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/CSVImporter.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/CSVImporter.java
@@ -7,53 +7,61 @@ import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
+import org.apache.log4j.Logger;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 
 public class CSVImporter {
+    private static final Logger logger = Logger.getLogger(CSVImporter.class);
 
     private static Float DEFAULT_COORDINATE_MIN_ACCURACY = 20f;
     private static Double DEFAULT_RDP_EPSILON = 0.0000001;
-    private static ConfigService config;
 
-    public static void main(String[] args) throws ArgumentParserException {
+    private static ConfigService config;
+    private static  ArgumentParser parser;
+
+    public static void main(String[] args) {
 
         config = new ConfigService();
         config.readProperties();
-
-
-        ArgumentParser parser = ArgumentParsers.newFor("CSVImporter").build()
-                .defaultHelp(true)
-                .description("Import CSV file.");
-        parser.addArgument("-t", "--type")
-                .choices("r", "p").setDefault("r")
-                .help("Specify file type r <- ride, p <- profile");
-        parser.addArgument("file").nargs("*")
-                .help("File to read");
-        parser.addArgument("-a", "--accuracy")
-                .help("Minimum accuracy of to be processed coordinates").setDefault(DEFAULT_COORDINATE_MIN_ACCURACY).type(Float.class);
-        parser.addArgument("-e", "--epsilon")
-                .help("Epsilon Parameter of RDP-Algorithm").setDefault(DEFAULT_RDP_EPSILON).type(Double.class);
         Namespace ns = null;
         try {
+            parser = ArgumentParsers.newFor("CSVImporter").build()
+                .defaultHelp(true)
+                .description("Import CSV file.");
+            parser.addArgument("-t", "--type")
+                    .choices("r", "p").setDefault("r")
+                    .help("Specify file type r <- ride, p <- profile");
+            parser.addArgument("file").nargs("*")
+                    .help("File to read");
+            parser.addArgument("-a", "--accuracy")
+                    .help("Minimum accuracy of to be processed coordinates").setDefault(DEFAULT_COORDINATE_MIN_ACCURACY).type(Float.class);
+            parser.addArgument("-e", "--epsilon")
+                    .help("Epsilon Parameter of RDP-Algorithm").setDefault(DEFAULT_RDP_EPSILON).type(Double.class);
+
             ns = parser.parseArgs(args);
-        } catch (ArgumentParserException e) {
-            parser.handleError(e);
+            String type = ns.getString("type");
+            if (type.isEmpty() || ns.<String>getList("file").isEmpty()) {
+                throw new ArgumentParserException(parser);
+            }
+            for (String name : ns.<String>getList("file")) {
+                Path path = Paths.get(name);
+                if (type.contains("p")) {
+                    new ProfileFileIOHandler(path);
+                } else if (type.contains("r")) {
+                    Float minAccuracy = ns.getFloat("accuracy");
+                    Double rdpEpsilon = ns.getDouble("epsilon");
+                    new RideFileIOHandler(path, minAccuracy, rdpEpsilon);
+                }
+            }
+
+        } catch (Exception e) {
+            parser.handleError((ArgumentParserException)e);
+            logger.error(e);
             System.exit(1);
         }
-        String type = ns.getString("type");
-        if (type.isEmpty()) throw new ArgumentParserException(parser);
-        for (String name : ns.<String>getList("file")) {
-            Path path = Paths.get(name);
-            if (type.contains("p")) {
-                new ProfileFileIOHandler(path);
-            } else if (type.contains("r")) {
-                Float minAccuracy = ns.getFloat("accuracy");
-                Double rdpEpsilon = ns.getDouble("epsilon");
-                new RideFileIOHandler(path, minAccuracy, rdpEpsilon);
-            }
-        }
+
     }
 }
