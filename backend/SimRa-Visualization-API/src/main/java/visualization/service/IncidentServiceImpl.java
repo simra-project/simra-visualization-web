5e98729ae42109849133b4f0569fed66c7b04fc4
Adrian Gronau <adrian.gronau@gmx.de>
2019-12-08 21:04:54 +0100

    Only outputting actual incidents (type != 0)

--- backend/SimRa-Visualization-API/src/main/java/visualization/service/IncidentServiceImpl.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/service/IncidentServiceImpl.java
@@ -10,15 +10,8 @@ import visualization.web.resources.IncidentResource;
 import visualization.web.resources.serializers.IncidentResourceMapper;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-
-/*
-
-This is the place where we can do some number crunching and other postprocessing for Incidents
-
- */
 @Service
 public class IncidentServiceImpl implements IncidentService {
 
@@ -28,48 +21,46 @@ public class IncidentServiceImpl implements IncidentService {
     @Autowired
     private IncidentResourceMapper incidentResourceMapper;
 
-
     @Override
     public IncidentResource getIncident(String rideId, String key) {
-
-        IncidentResource incidentResource;
         IncidentEntity.CompositeKey compositeKey = new IncidentEntity.CompositeKey(rideId, key);
 
-        Optional<IncidentEntity> optionalIncidentEntity = incidentRepository.findById(compositeKey);
-        IncidentEntity incidentEntity = optionalIncidentEntity.get();
-        incidentResource = incidentResourceMapper.mapEntityToResource(incidentEntity);
+        IncidentEntity incidentEntity = incidentRepository.findById(compositeKey).get();
 
-        return incidentResource;
+        return incidentResourceMapper.mapEntityToResource(incidentEntity);
     }
 
     @Override
     public List<IncidentResource> getIncidentsByFileId(String rideId) {
-
-        List<IncidentResource> incidents;
         List<IncidentEntity> incidentEntities = incidentRepository.findByFileId(rideId);
-        incidents = incidentEntities.stream().map(incidentEntity -> incidentResourceMapper.mapEntityToResource(incidentEntity)).collect(Collectors.toList());
-        return incidents;
+
+        return incidentEntities.stream()
+                .filter(incident -> incident.getIncident() != 0)
+                .map(incident -> incidentResourceMapper.mapEntityToResource(incident))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<IncidentResource> getIncidentsInRange(double longitude, double latitude, int maxDistance) {
-
         GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
 
-        List<IncidentResource> incidents;
         List<IncidentEntity> incidentEntities = incidentRepository.findByLocationNear(point, maxDistance);
-        incidents = incidentEntities.stream().map(incidentEntity -> incidentResourceMapper.mapEntityToResource(incidentEntity)).collect(Collectors.toList());
-        return incidents;
+
+        return incidentEntities.stream()
+                .filter(incident -> incident.getIncident() != 0)
+                .map(incident -> incidentResourceMapper.mapEntityToResource(incident))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<IncidentResource> getIncidentsInWithin(GeoJsonPoint first, GeoJsonPoint second, GeoJsonPoint third, GeoJsonPoint fourth) {
-
         GeoJsonPolygon polygon = new GeoJsonPolygon(first, second, third, fourth, first);
-        List<IncidentResource> incidents;
-        List<IncidentEntity> incidentEntities = incidentRepository.findByLocationWithin(polygon);
-        incidents = incidentEntities.stream().map(incidentEntity -> incidentResourceMapper.mapEntityToResource(incidentEntity)).collect(Collectors.toList());
-        return incidents;
-    }
 
+        List<IncidentEntity> incidentEntities = incidentRepository.findByLocationWithin(polygon);
+
+        return incidentEntities.stream()
+                .filter(incident -> incident.getIncident() != 0)
+                .map(incident -> incidentResourceMapper.mapEntityToResource(incident))
+                .collect(Collectors.toList());
+    }
 }
--- backend/SimRa-Visualization-API/src/main/java/visualization/service/RideServiceImpl.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/service/RideServiceImpl.java
@@ -10,15 +10,8 @@ import visualization.web.resources.RideResource;
 import visualization.web.resources.serializers.RideResourceMapper;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
-
-/*
-
-This is the place where we can do some number crunching and other postprocessing for Rides
-
- */
 @Service
 public class RideServiceImpl implements RideService {
 
@@ -30,40 +23,31 @@ public class RideServiceImpl implements RideService {
 
     @Override
     public RideResource getRideById(String rideId) {
+        RideEntity rideEntity = rideRepository.findById(rideId).get();
 
-        Optional<RideEntity> optionalRideEntity = rideRepository.findById(rideId);
-        RideEntity rideEntity = optionalRideEntity.get();
-        RideResource rideResource = rideResourceMapper.mapRideEntityToResource(rideEntity);
-
-        return rideResource;
+        return rideResourceMapper.mapRideEntityToResource(rideEntity);
     }
 
     @Override
     public List<RideResource> getRidesWithin(GeoJsonPoint first, GeoJsonPoint second, GeoJsonPoint third, GeoJsonPoint fourth) {
-
         GeoJsonPolygon polygon = new GeoJsonPolygon(first, second, third, fourth, first);
         List<RideEntity> rideEntities = rideRepository.findByLocationWithin(polygon);
-        List<RideResource> rideResources = rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
 
-        return rideResources;
+        return rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
     }
 
     @Override
     public List<RideResource> getRidesInRange(double latitude, double longitude, int maxDistance) {
         GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
         List<RideEntity> rideEntities = rideRepository.findByLocationNear(point, maxDistance);
-        List<RideResource> rideResources = rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
 
-        return rideResources;
+        return rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
     }
 
     @Override
     public List<RideResource> getRidesAtTime(Long fromTs, Long untilTs) {
-
         List<RideEntity> rideEntities = rideRepository.findAllByTsBetween(fromTs, untilTs);
-        List<RideResource> rideResources = rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
 
-        return rideResources;
+        return rideEntities.stream().map(rideEntity -> rideResourceMapper.mapRideEntityToResource(rideEntity)).collect(Collectors.toList());
     }
-
 }
