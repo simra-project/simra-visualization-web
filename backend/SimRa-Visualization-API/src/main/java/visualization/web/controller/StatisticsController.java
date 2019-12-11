a19681bc3209c4bd5b76323a35fdd8659ff48810
sehof-git <se@anhofmann.de>
2019-12-11 15:28:58 +0100

    adapted Statistics Service

--- backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/StatisticsController.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/web/controller/StatisticsController.java
@@ -17,24 +17,7 @@ public class StatisticsController {
     @Autowired
     private StatisticsService statisticsService;
 
-    /*@GetMapping(value = "/statistics")
-    public HttpEntity<String> getStatistics(@RequestParam(value = "region") String region) {
-        MongoDatabase db = (new MongoClient("localhost", 27017)).getDatabase("simra");
-        MongoCollection<Document> collection = db.getCollection("statistics");
-
-        Document statistic = collection.find(eq("region", region)).sort(new BasicDBObject("timestamp", -1)).limit(1).first();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_TYPE, "application/json; charset=UTF-8");
-
-        if (statistic != null) {
-            return ResponseEntity.ok().headers(headers).body(statistic.toJson());
-        } else {
-            return ResponseEntity.status(404).headers(headers).body("{ \"error\": \"No statistic found for this region.\" }");
-        }
-    }*/
-
-    @GetMapping(value = "/statisticsnew")
+    @GetMapping(value = "/statistics")
     public HttpEntity<StatisticsResource> getStatisticsByRegion(@RequestParam(value = "region") String region) {
         return ResponseEntity.ok(statisticsService.getStatisticsByRegion(region));
     }
