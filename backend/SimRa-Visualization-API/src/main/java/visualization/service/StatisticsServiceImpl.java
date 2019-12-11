13e975e8428b0542ca9395ce914782f2fb402f49
sehof-git <se@anhofmann.de>
2019-12-11 15:27:33 +0100

    added Entity and Repo Format

--- backend/SimRa-Visualization-API/src/main/java/visualization/service/StatisticsServiceImpl.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/service/StatisticsServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import visualization.data.mongodb.StatisticsRepository;
 import visualization.data.mongodb.entities.StatisticsEntity;
 import visualization.web.resources.StatisticsResource;
+import visualization.web.resources.serializers.StatisticsResourceMapper;
 
 @Service
 public class StatisticsServiceImpl implements StatisticsService {
@@ -12,10 +13,12 @@ public class StatisticsServiceImpl implements StatisticsService {
     @Autowired
     private StatisticsRepository statisticsRepository;
 
+    @Autowired
+    private StatisticsResourceMapper statisticsResourceMapper;
+
     @Override
     public StatisticsResource getStatisticsByRegion(String region) {
-        StatisticsEntity statisticsEntity = statisticsRepository.findByRegion(region).get();
-        //TBD
-        return null;
+        StatisticsEntity statisticsEntity = statisticsRepository.findTop1ByRegionOrderByTimestampDesc(region, 0).get();
+        return statisticsResourceMapper.mapRideEntityToResource(statisticsEntity);
     }
 }
--- backend/SimRa-Visualization-API/src/main/java/visualization/web/resources/serializers/StatisticsResourceMapper.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/web/resources/serializers/StatisticsResourceMapper.java
@@ -1,4 +1,41 @@
 package visualization.web.resources.serializers;
 
+import org.springframework.stereotype.Component;
+import visualization.data.mongodb.entities.StatisticsEntity;
+import visualization.web.resources.StatisticsResource;
+
+@Component
 public class StatisticsResourceMapper {
+    public StatisticsResource mapRideEntityToResource(StatisticsEntity statisticsEntity) {
+
+        StatisticsResource statisticsResource = new StatisticsResource();
+
+        statisticsResource.setId(statisticsEntity.getId());
+        statisticsResource.setRegion(statisticsEntity.getRegion());
+        statisticsResource.setTimestamp(statisticsEntity.getTimestamp());
+        statisticsResource.setRideCount(statisticsEntity.getRideCount());
+        statisticsResource.setAccumulatedDistance(statisticsEntity.getAccumulatedDistance());
+        statisticsResource.setAccumulatedDuration(statisticsEntity.getAccumulatedDistance());
+        statisticsResource.setAccumulatedSavedCO2(statisticsEntity.getAccumulatedDuration());
+        statisticsResource.setAverageDuration(statisticsEntity.getAverageDuration());
+        statisticsResource.setAverageDistance(statisticsEntity.getAverageDistance());
+        statisticsResource.setAverageSpeed(statisticsEntity.getAverageSpeed());
+        statisticsResource.setIncidentCount(statisticsEntity.getIncidentCount());
+        statisticsResource.setIncidentCountScary(statisticsEntity.getIncidentCountScary());
+        statisticsResource.setIncidentCountWithChildrenInvolved(statisticsEntity.getIncidentCountWithChildrenInvolved());
+        statisticsResource.setIncidentCountWithTrailersInvolved(statisticsEntity.getIncidentCountWithTrailersInvolved());
+        statisticsResource.setIncidentParticipantTypeData(statisticsEntity.getIncidentParticipantTypeData());
+        statisticsResource.setIncidentParticipantTypeLabels(statisticsEntity.getIncidentParticipantTypeLabels());
+        statisticsResource.setIncidentTypeData(statisticsEntity.getIncidentTypeData());
+        statisticsResource.setIncidentTypeLabels(statisticsEntity.getIncidentTypeLabels());
+        statisticsResource.setProfileAgeDistributionLabels(statisticsEntity.getProfileAgeDistributionLabels());
+        statisticsResource.setProfileBikeTypeData(statisticsEntity.getProfileBikeTypeData());
+        statisticsResource.setProfileBikeTypeLabels(statisticsEntity.getProfileBikeTypeLabels());
+        statisticsResource.setProfileCount(statisticsEntity.getProfileCount());
+        statisticsResource.setProfileCountFemale(statisticsEntity.getProfileCountFemale());
+        statisticsResource.setProfileCountMale(statisticsEntity.getProfileCountMale());
+        statisticsResource.setProfileCountOther(statisticsEntity.getProfileCountOther());
+
+        return statisticsResource;
+    }
 }
