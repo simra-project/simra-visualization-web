4acc1462f5980c9f75914037cc5bdd943d008686
sehof-git <se@anhofmann.de>
2019-12-11 15:27:33 +0100

    added Entity and Repo Format

--- backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/StatisticsRepository.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/data/mongodb/StatisticsRepository.java
@@ -1,6 +1,7 @@
 package visualization.data.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import visualization.data.mongodb.entities.StatisticsEntity;
 
 import java.util.Optional;
@@ -8,6 +9,6 @@ import java.util.Optional;
 
 public interface StatisticsRepository extends MongoRepository<StatisticsEntity, String> {
 
-    Optional<StatisticsEntity> findByRegion(String region);
+    Optional<StatisticsEntity> findTop1ByRegionOrderByTimestampDesc(String region, int limit);
 
 }
--- backend/SimRa-Visualization-API/src/main/java/visualization/web/resources/StatisticsResource.java
+++ backend/SimRa-Visualization-API/src/main/java/visualization/web/resources/StatisticsResource.java
@@ -1,7 +1,11 @@
 package visualization.web.resources;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
 
 @Getter
 @Setter
@@ -11,8 +15,78 @@ import lombok.*;
 @JsonInclude
 public class StatisticsResource {
 
-    //tbd
+    @JsonProperty
     private String id;
 
+    @JsonProperty
     private String region;
+
+    @JsonProperty
+    private String timestamp;
+
+    @JsonProperty
+    private int rideCount;
+
+    @JsonProperty
+    private Double accumulatedDuration;
+
+    @JsonProperty
+    private Double accumulatedDistance;
+
+    @JsonProperty
+    private Double accumulatedSavedCO2;
+
+    @JsonProperty
+    private Long averageDuration;
+
+    @JsonProperty
+    private Double averageDistance;
+
+    @JsonProperty
+    private Double averageSpeed;
+
+    @JsonProperty
+    private int incidentCount;
+
+    @JsonProperty
+    private ArrayList<String> incidentTypeLabels;
+
+    @JsonProperty
+    private ArrayList<Integer> incidentTypeData;
+
+    @JsonProperty
+    private ArrayList<String> incidentParticipantTypeLabels;
+
+    @JsonProperty
+    private ArrayList<Integer> incidentParticipantTypeData;
+
+    @JsonProperty
+    private int incidentCountWithChildrenInvolved;
+
+    @JsonProperty
+    private int incidentCountWithTrailersInvolved;
+
+    @JsonProperty
+    private int incidentCountScary;
+
+    @JsonProperty
+    private int profileCount;
+
+    @JsonProperty
+    private int profileCountMale;
+
+    @JsonProperty
+    private int profileCountFemale;
+
+    @JsonProperty
+    private int profileCountOther;
+
+    @JsonProperty
+    private ArrayList<String> profileBikeTypeLabels;
+
+    @JsonProperty
+    private ArrayList<Integer> profileBikeTypeData;
+
+    @JsonProperty
+    private ArrayList<String> profileAgeDistributionLabels;
 }
