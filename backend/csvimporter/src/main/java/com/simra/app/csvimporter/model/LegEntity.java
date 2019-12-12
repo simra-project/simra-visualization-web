587d8bd7490698537af85e10ced75e10fb67ebea
kons <kons.koehler@gmail.com>
2019-12-12 10:59:22 +0100

    remove unnecessary serializer

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/model/LegEntity.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/model/LegEntity.java
@@ -1,7 +1,5 @@
 package com.simra.app.csvimporter.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.simra.app.csvimporter.model.serializer.GeoJsonLineStringSerializer;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.geo.GeoJsonLineString;
@@ -20,7 +18,6 @@ public class LegEntity {
 
     private String type = "Feature";
 
-    @JsonSerialize(using = GeoJsonLineStringSerializer.class)
     private GeoJsonLineString geometry;
 
     private LegPropertyEntity properties;
