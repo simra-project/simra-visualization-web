e0bc98917f37bcbfd8fbfa99c13a9a008baab209
spandey <SanjeetRaj.Pandey@init.de>
2020-01-10 12:42:55 +0100

    * matched location and incident location field index auto setup.

--- backend/csvimporter/src/main/java/com/simra/app/csvimporter/CsvimporterApplication.java
+++ backend/csvimporter/src/main/java/com/simra/app/csvimporter/CsvimporterApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeospatialIndex;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CsvimporterApplication implements CommandLineRunner {
@@ -16,6 +19,9 @@ public class CsvimporterApplication implements CommandLineRunner {
 
     @Autowired
     private MonitorService monitorService;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
 
     public static void main(String[] args) {
@@ -29,6 +35,19 @@ public class CsvimporterApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        GeospatialIndex geospatialIndexLocation=new GeospatialIndex("locationMapMatched");
+        geospatialIndexLocation.typed(GeoSpatialIndexType.GEO_2DSPHERE);
+        this.mongoTemplate.indexOps("rides").ensureIndex(geospatialIndexLocation);
+        logger.info("Rides Location Indexed setup.");
+
+        GeospatialIndex geospatialIndexIncident=new GeospatialIndex("location");
+        geospatialIndexIncident.typed(GeoSpatialIndexType.GEO_2DSPHERE);
+        this.mongoTemplate.indexOps("incidents").ensureIndex(geospatialIndexIncident);
+        logger.info("Incident Location Indexed setup.");
+
+
+
         monitorService.startRecursiveWatcher();
     }
 
