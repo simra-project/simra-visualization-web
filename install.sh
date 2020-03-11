4edc14147acd0fbb08ef5c6760d74c170204146b
Richard <richard.viehoever@gmail.com>
2020-03-11 14:45:05 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#get info from user
+echo Please enter server name, ex: yourserver.com:
+read servername
+echo Please specify the directory to be monitored by the importer:
+read monitordir
+echo Please specify the logging directory:
+read loggingdir
+echo Please specify the user you want to run the java applications on:
+read javauser
+
+#install nginx
+sudo apt update
+sudo apt install nginx -y
+
+#install java + maven
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo apt install maven -y
+
+#install nodejs
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+
+#install mongodb
+. /etc/lsb-release
+sudo apt-get install gnupg -y
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+if [ "$DISTRIB_CODENAME" = "xenial" ]
+then
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+else
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+fi
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+
+#install git
+sudo apt install git -y
+
+#clone repository
+git clone https://github.com/simra-project/SimRa-Visualization.git
+cd SimRa-Visualization
+
+#compile jars
+mvn clean install -DskipTests -U
+
+#import dump
+mongorestore
+
+#compile frontend
+cd frontend
+npm install
+npm run build
+cd ..
+
+#configure and start frontend
+cat <<EOT > /etc/nginx/sites-enabled/simra.conf
+server {
+  listen 80;
+  server_name $servername;
+
+  # Tell Nginx where your app's 'public' directory is
+  root `pwd`/frontend/dist;
+
+  location / {
+	try_files \$uri \$uri/ /index.html;
+  }
+}
+EOT
+sudo service nginx restart
+
+#configure and start interface
+cat <<EOT > /etc/systemd/system/simra_api.service
+[Unit]
+Description=Simra Springboot API
+After=syslog.target
+After=network.target[Service]
+User=$javauser
+Type=simple
+
+[Service]
+ExecStart=/usr/bin/java -jar `pwd`/backend/SimRa-Visualization-API/target/SimRa-Visualization-API-1.0-SNAPSHOT.jar
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=simra_api
+
+[Install]
+WantedBy=multi-user.target
+EOT
+sudo systemctl start simra_api
+
+#configure and start backend
+cat <<EOT >> backend/csvimporter/src/main/resources/application.properties
+csv.monitor.path=$monitorpath
+logging.file.name=$loggingpath/csvimporter.log
+EOT
+
+cat <<EOT > /etc/systemd/system/simra_backend.service
+[Unit]
+Description=Simra Springboot API
+After=syslog.target
+After=network.target[Service]
+User=$javauser
+Type=simple
+
+[Service]
+ExecStart=/usr/bin/java -jar `pwd`/backend/csvimporter/target/csvimporter-0.0.1-SNAPSHOT.jar
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=simra_backend
+
+[Install]
+WantedBy=multi-user.target
+EOT
+sudo systemctl start simra_backend
