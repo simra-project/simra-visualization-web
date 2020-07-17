b6944f4935a7a3b4e49d4742ab583393285be60f
selphiron <ahmetserdar@hotmail.de>
2020-07-17 11:43:32 +0200

    Create alternative_install.sh

---
+++ alternative_install.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+set -e
+
+#get info from user
+echo Please enter server name, ex: yourserver.com:
+read servername
+echo Please specify the directory to be monitored by the importer:
+read monitorpath
+echo Please specify the directory for the pbf files:
+read pbffiles
+echo Please specify the logging directory:
+read loggingdir
+echo Please specify the user you want to run the java applications on:
+read javauser
+
+#add required repositories
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt update
+
+#install nginx
+sudo apt install nginx -y
+
+#install java + maven
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo apt install maven -y
+
+#install nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#install mongodb
+. /etc/lsb-release
+sudo apt-get install gnupg -y
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+if [ "$DISTRIB_CODENAME" = "xenial" ]
+then
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+else
+	echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+fi
+sudo apt update
+sudo apt install mongodb-org -y
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+
+#install git
+sudo apt install git -y
+
+#clone repository
+git clone https://github.com/simra-project/SimRa-Visualization.git
+cd SimRa-Visualization
+
+#update configuration of CSV importer
+sudo cat <<EOT >> backend/csvimporter/src/main/resources/application.properties
+csv.monitor.path=$monitorpath
+pbf.path=$pbffiles
+logging.file.name=$loggingdir/csvimporter.log
+EOT
+
+#compile jars
+sudo mvn clean install -DskipTests
+
+#import dump
+mongorestore
+
+#compile frontend
+cd frontend
+npm install
+npm run build
+cd ..
+
+#configure and start frontend
+sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 256;/g' /etc/nginx/nginx.conf
+sudo cat <<EOT > ./simra.conf
+server {
+  listen 80;
+  server_name $servername;
+
+  # Tell Nginx where your app's 'public' directory is
+  root `pwd`/frontend/dist;
+
+  location / {
+	try_files \$uri \$uri/ /index.html;
+  }
+}
+EOT
+sudo mv ./simra.conf /etc/nginx/sites-enabled/simra.conf
+sudo service nginx restart
+
+#configure and start interface
+sudo cat <<EOT > ./simra_api.service
+[Unit]
+Description=Simra Springboot API
+After=syslog.target
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/java -jar `pwd`/backend/SimRa-Visualization-API/target/SimRa-Visualization-API-1.0-SNAPSHOT.jar
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=simra_api
+User=$javauser
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOT
+sudo mv ./simra_api.service /etc/systemd/system/simra_api.service
+sudo systemctl start simra_api
+
+#configure and start backend
+
+sudo cat <<EOT > ./simra_backend.service
+[Unit]
+Description=Simra Springboot API
+After=syslog.target
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/java -jar `pwd`/backend/csvimporter/target/csvimporter-0.0.1-SNAPSHOT.jar
+Restart=always
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=simra_backend
+User=$javauser
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOT
+sudo mv ./simra_backend.service /etc/systemd/system/simra_backend.service
+sudo systemctl start simra_backend
