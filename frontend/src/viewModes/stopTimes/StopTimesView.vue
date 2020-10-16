23c8382c10114da785ce17dc161a9cac4d630bd3
Adrian Gronau <adrian.gronau@gmx.de>
2020-10-16 11:57:46 +0200

    Fixed stop times tiles

--- frontend/src/viewModes/stopTimes/StopTimesView.vue
+++ frontend/src/viewModes/stopTimes/StopTimesView.vue
@@ -1,5 +1,5 @@
 <template>
-    <l-tile-layer url="http://207.180.205.80:1337/tiles/simra_junctions_stoptimes/{z}/{x}/{y}.png"/>
+    <l-tile-layer url="http://207.180.205.80:1337/tiles/junctions-stoptimes/{z}/{x}/{y}.png"/>
 </template>
 
 <script>
