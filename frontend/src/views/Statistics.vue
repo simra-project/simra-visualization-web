88c15ba383919e44e9ad39288f07531a0e1f42ec
Adrian Gronau <adrian.gronau@gmx.de>
2020-11-17 18:23:49 +0100

    Use new api for statistics

--- frontend/src/views/Statistics.vue
+++ frontend/src/views/Statistics.vue
@@ -9,23 +9,23 @@
             </b-select>
         </h2>
 
-        <div class="wrapper" v-if="statistics.accumulatedDistance != null">
+        <div class="wrapper" v-if="statistics.r_meters != null">
             <div class="top-text">
                 Over
                 <span class="highlight-text">
-                    <ICountUp :delay="100" :endVal="statistics.profileCount"/> bikers
+                    <ICountUp :delay="100" :endVal="statistics.p_count"/> bikers
                 </span>
                 in {{ selectedLocation }} cycled
                 <span class="highlight-text">
-                    <ICountUp :delay="400" :endVal="Math.floor(statistics.accumulatedDistance / 1000)"/> km
+                    <ICountUp :delay="400" :endVal="Math.floor(statistics.r_meters / 1000)"/> km
                 </span>
                 so far reducing CO<sub>2</sub> emissions by
                 <span class="highlight-text">
                     <ICountUp :delay="700" :endVal="Math.floor(3781.43)"/> kg</span>.
             </div>
             <div class="top-subtext">
-                On average, one ride is {{ (statistics.averageDistance / 1000).toFixed(2) }} kilometers long and lasts {{ Math.floor(statistics.averageDuration / (1000 * 60)) }} minutes.
-                That's a speed of {{ statistics.averageSpeed.toFixed(1) }} km/h on average.
+                On average, one ride is {{ (statistics.r_avg_distance / 1000).toFixed(2) }} kilometers long and lasts {{ Math.floor(statistics.r_avg_duration / (1000 * 60)) }} minutes.
+                That's a speed of {{ statistics.r_avg_velocity.toFixed(1) }} km/h on average.
             </div>
 
             <hr style="margin-bottom: 2.5rem;">
@@ -42,7 +42,7 @@
                             <b-tag rounded>?</b-tag>
                         </b-tooltip>
                     </h4>
-                    <apexchart type=donut width=100% :options="{...chartOptions(['Not scary', 'Scary']), colors: ['#3f51b5', '#e52e71'] }" :series="[statistics.incidentCount, statistics.incidentCountScary]"/>
+                    <apexchart type=donut width=100% :options="{...chartOptions(['Not scary', 'Scary']), colors: ['#3f51b5', '#e52e71'] }" :series="[statistics.i_count, statistics.i_count_scary]"/>
                 </div>
                 <div class="column">
                     <h4>Participants
@@ -71,87 +71,8 @@
                     <apexchart type=bar width=100% :options="ageDistributionOptions" :series="ageDistributionData"/>
                 </div>
                 <div class="column">
-                    <apexchart type=donut width=100% :options="chartOptions(['Male', 'Female', 'Other'])" :series="[statistics.profileCountMale, statistics.profileCountFemale, statistics.profileCountOther]"/>
+                    <apexchart type=donut width=100% :options="chartOptions(['Male', 'Female', 'Other'])" :series="[statistics.p_gender_male, statistics.p_gender_female, statistics.p_gender_other]"/>
                 </div>
-            </div>
-
-<!--            <hr>-->
-
-<!--            <h3>Raw data</h3>-->
-<!--            <table class="table is-striped is-hoverable is-bordered is-narrow">-->
-<!--                <thead>-->
-<!--                <tr>-->
-<!--                    <th>Age Group</th>-->
-<!--                    <th style="white-space: nowrap;"># Bikers</th>-->
-<!--                    <th style="white-space: nowrap;"># Rides</th>-->
-<!--                    <th>Total Distance</th>-->
-<!--                    <th>Total Duration</th>-->
-<!--                    <th>Average Distance</th>-->
-<!--                    <th>Average Duration</th>-->
-<!--                    <th>Average Speed</th>-->
-<!--                    <th>#&nbsp;Scary Incidents</th>-->
-<!--                    <th>Average # Scary Incidents</th>-->
-<!--                    <th>Total CO<sub>2</sub> saved</th>-->
-<!--                    <th>Average CO<sub>2</sub> saved</th>-->
-<!--                </tr>-->
-<!--                </thead>-->
-<!--                <tbody>-->
-<!--                <tr v-for="row in statistics.profileAgeGroupCrossData">-->
-<!--                    <td><strong>{{ row.ageGroup }}</strong></td>-->
-<!--                    <td>{{ row.bikerCount.toLocaleString('en') }}</td>-->
-<!--                    <td>{{ row.rideCount.toLocaleString('en') }}</td>-->
-
-<!--                    <td>{{ Math.floor(row.accumulatedDistance / 1000).toLocaleString('en') }} km</td>-->
-<!--                    <td>{{ Math.floor(row.accumulatedDuration / (1000 * 60 * 60)).toLocaleString('en') }} h</td>-->
-
-<!--                    <template v-if="row.rideCount > 0">-->
-<!--                        <td>{{ (row.averageDistance / 1000).toFixed(2) }} km</td>-->
-<!--                        <td>{{ Math.floor(row.averageDuration / (1000 * 60)) }} min</td>-->
-<!--                        <td>{{ row.averageSpeed.toFixed(1) }} km/h</td>-->
-<!--                    </template>-->
-<!--                    <template v-else>-->
-<!--                        <td>-</td>-->
-<!--                        <td>-</td>-->
-<!--                        <td>-</td>-->
-<!--                    </template>-->
-
-<!--                    <td>{{ row.scaryIncidentCount.toLocaleString('en') }}</td>-->
-<!--                    <td>{{ row.rideCount > 0 ? row.averageScaryIncidentCount.toFixed(1).toLocaleString('en') : '-' }}</td>-->
-
-<!--                    <td>{{ Math.floor(row.accumulatedSavedCO2).toLocaleString('en') }} kg</td>-->
-<!--                    <td>{{ row.rideCount > 0 ? (Math.floor(row.averageSavedCO2).toLocaleString('en') + ' kg') : '-' }}</td>-->
-<!--                </tr>-->
-<!--                </tbody>-->
-<!--                <tfoot>-->
-<!--                <tr>-->
-<!--                    <th><strong>Total</strong></th>-->
-<!--                    <th>{{ statistics.profileAgeGroupCrossTotal.bikerCount.toLocaleString('en') }}</th>-->
-<!--                    <th>{{ statistics.profileAgeGroupCrossTotal.rideCount.toLocaleString('en') }}</th>-->
-
-<!--                    <th>{{ Math.floor(statistics.profileAgeGroupCrossTotal.accumulatedDistance / 1000).toLocaleString('en') }} km</th>-->
-<!--                    <th>{{ Math.floor(statistics.profileAgeGroupCrossTotal.accumulatedDuration / (1000 * 60 * 60)).toLocaleString('en') }} h</th>-->
-
-<!--                    <th>{{ (statistics.profileAgeGroupCrossTotal.averageDistance / 1000).toFixed(2) }} km</th>-->
-<!--                    <th>{{ Math.floor(statistics.profileAgeGroupCrossTotal.averageDuration / (1000 * 60)) }} min</th>-->
-<!--                    <th>{{ statistics.profileAgeGroupCrossTotal.averageSpeed.toFixed(1) }} km/h</th>-->
-
-<!--                    <th>{{ statistics.profileAgeGroupCrossTotal.scaryIncidentCount.toLocaleString('en') }}</th>-->
-<!--                    <th>{{ statistics.profileAgeGroupCrossTotal.averageScaryIncidentCount.toFixed(1).toLocaleString('en') }}</th>-->
-
-<!--                    <th>{{ Math.floor(statistics.profileAgeGroupCrossTotal.accumulatedSavedCO2).toLocaleString('en') }} kg</th>-->
-<!--                    <th>{{ Math.floor(statistics.profileAgeGroupCrossTotal.averageSavedCO2).toLocaleString('en') }} kg</th>-->
-<!--                </tr>-->
-<!--                </tfoot>-->
-<!--            </table>-->
-
-            <hr>
-
-            <div class="bottom-info-text">
-                The rides we store have no references left to the original user who sent it to us.<br>
-                If you are a bicyclist too, download our app and start tracking!<br>
-                <a class="button is-large is-light" href="https://www.mcc.tu-berlin.de/menue/forschung/projekte/simra/">
-                    <strong>Download App</strong>
-                </a>
             </div>
         </div>
         <div class="wrapper" v-else style="height: 300px">
@@ -163,6 +84,7 @@
 <script>
 import VueApexCharts from "vue-apexcharts";
 import ICountUp from "vue-countup-v2";
+import {IncidentUtils} from "@/services/IncidentUtils";
 
 export default {
     name: "statistics",
@@ -220,21 +142,40 @@ export default {
             this.dataLoaded = false;
 
             setTimeout(() => {
-                fetch(process.env.VUE_APP_BACKEND_URL + "/statistics?region=" + this.selectedLocation)
+                // fetch(process.env.VUE_APP_BACKEND_URL + "/statistics?region=" + this.selectedLocation)
+                fetch("http://207.180.205.80:8000/api/statistics/ZES_Experimentell/")
                     .then(r => r.json())
                     .then(r => {
                         this.dataLoaded = true;
                         this.statistics = r;
 
-                        this.incidentTypes = this.processData(4, r.incidentTypeLabels, r.incidentTypeData);
-                        this.participantTypes = this.processData(4, r.incidentParticipantTypeLabels, r.incidentParticipantTypeData);
-                        this.bikeTypes = this.processData(4, r.profileBikeTypeLabels, r.profileBikeTypeData);
+                        let incidentData = [r.i_incident_0, r.i_incident_1, r.i_incident_2, r.i_incident_3, r.i_incident_4, r.i_incident_5, r.i_incident_6, r.i_incident_7, r.i_incident_8];
+                        this.incidentTypes = this.processData(4, IncidentUtils.getTypes().map(x => x.name), incidentData);
 
-                        this.ageDistributionOptions.xaxis.categories = r.profileAgeDistributionLabels;
+                        let participantData = [/*r.i_itype_0,*/ r.i_itype_1, r.i_itype_2, r.i_itype_3, r.i_itype_4, r.i_itype_5, r.i_itype_6, r.i_itype_7, r.i_itype_8, r.i_itype_9, r.i_itype_10];
+                        this.participantTypes = this.processData(4, IncidentUtils.getParticipants().map(x => x.name), participantData);
+
+                        let bikeData = [r.i_biketype_0, r.i_biketype_1, r.i_biketype_2, r.i_biketype_3, r.i_biketype_4, r.i_biketype_5, r.i_biketype_6, r.i_biketype_7, r.i_biketype_8];
+                        this.bikeTypes = this.processData(4, IncidentUtils.getBikeTypes().map(x => x.name), bikeData);
+
+                        this.ageDistributionOptions.xaxis.categories = [
+                            "> 2004",
+                            "2000–2004",
+                            "1995–1999",
+                            "1990–1994",
+                            "1985–1989",
+                            "1980–1984",
+                            "1975–1979",
+                            "1970–1974",
+                            "1965–1969",
+                            "1960–1964",
+                            "1955–1959",
+                            "1950–1954",
+                            "< 1950"];
                         this.ageDistributionData = [
-                            { name: "Male", data: r.profileAgeDistributionDataMale },
-                            { name: "Female", data: r.profileAgeDistributionDataFemale },
-                            { name: "Other", data: r.profileAgeDistributionDataOther },
+                            { name: "Male", data: [r.p_birth_1_male, r.p_birth_2_male, r.p_birth_3_male, r.p_birth_4_male, r.p_birth_5_male, r.p_birth_6_male, r.p_birth_7_male, r.p_birth_8_male, r.p_birth_9_male, r.p_birth_10_male, r.p_birth_11_male, r.p_birth_12_male, r.p_birth_13_male] },
+                            { name: "Female", data: [r.p_birth_1_female, r.p_birth_2_female, r.p_birth_3_female, r.p_birth_4_female, r.p_birth_5_female, r.p_birth_6_female, r.p_birth_7_female, r.p_birth_8_female, r.p_birth_9_female, r.p_birth_10_female, r.p_birth_11_female, r.p_birth_12_female, r.p_birth_13_female] },
+                            { name: "Other", data: [r.p_birth_1_other, r.p_birth_2_other, r.p_birth_3_other, r.p_birth_4_other, r.p_birth_5_other, r.p_birth_6_other, r.p_birth_7_other, r.p_birth_8_other, r.p_birth_9_other, r.p_birth_10_other, r.p_birth_11_other, r.p_birth_12_other, r.p_birth_13_other] },
                         ];
                     });
             }, 500);
